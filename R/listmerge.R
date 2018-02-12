537c4775eb168394fff0d5421eb8e567e1857f87
Elpsytao <mitarai1992@gmail.com>
2018-02-12 16:56:01 -0500

    changes to listmerge

--- R/listmerge.R
+++ R/listmerge.R
@@ -3,18 +3,26 @@
 #' Two catagorized columns will be names of the list and names of each elements in list, values will be values in each element
 #' 
 #' listmerge:
-#' 1. Deal with collapse function
+#' 1. Deal with collapse (multiple namse in a list)
 #' 2. Deal with names match
 #' 3. Keep left/right/both
-#' 
+#' 4. cpp
 
 
-listmerge <- function(x, y, fun, use.names = F, keep){
+listmerge <- function(x, y, fun, use.names = F, keep = both){
   if(!is.list(x) || !is.list(y)){
     stop('Input must be lists.')
   }
-  all_names <- unique(c(names(x), names(y)))
-  out <- mapply(c, x[all_names], y[all_names])
-  names(out) <- all_names
+  both <- unique(c(names(x), names(y)))
+  names(both) <- both
+  out <- mapply(c, x[both], y[both])
+  names(out) <- both
+  out  <- out[names(eval(keep))]
   out
+  # if(keep == 'all'){
+  #   return(out)
+  # } else {
+  #   out  <- out[names(eval(as.name(keep)))]
+  #   return(out)
+  # }
 }
